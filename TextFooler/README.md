cfbcdf43012d61e06d1c5d8f24825082cd254945
Bill Yan <bxyan5@gmail.com>
2021-02-22 21:30:57 -0800

    added other files

---
+++ TextFooler/README.md
@@ -0,0 +1,65 @@
+# TextFooler
+A Model for Natural Language Attack on Text Classification and Inference
+
+This is the source code for the paper: [Jin, Di, et al. "Is BERT Really Robust? Natural Language Attack on Text Classification and Entailment." arXiv preprint arXiv:1907.11932 (2019)](https://arxiv.org/pdf/1907.11932.pdf). If you use the code, please cite the paper:
+
+```
+@article{jin2019bert,
+  title={Is BERT Really Robust? Natural Language Attack on Text Classification and Entailment},
+  author={Jin, Di and Jin, Zhijing and Zhou, Joey Tianyi and Szolovits, Peter},
+  journal={arXiv preprint arXiv:1907.11932},
+  year={2019}
+}
+```
+
+## Data
+Our 7 datasets are [here](https://bit.ly/nlp_adv_data).
+
+## Prerequisites:
+Required packages are listed in the requirements.txt file:
+```
+pip install requirements.txt
+```
+
+## How to use
+
+* Run the following code to install the **esim** package:
+
+ ```
+cd ESIM
+python setup.py install
+cd ..
+```
+
+* (Optional) Run the following code to pre-compute the cosine similarity scores between word pairs based on the [counter-fitting word embeddings](https://drive.google.com/open?id=1bayGomljWb6HeYDMTDKXrh0HackKtSlx).
+
+```
+python comp_cos_sim_mat.py [PATH_TO_COUNTER_FITTING_WORD_EMBEDDINGS]
+```
+
+* Run the following code to generate the adversaries for text classification:
+
+```
+python attack_classification.py
+```
+
+For Natural langauge inference:
+
+```
+python attack_nli.py
+```
+
+Examples of run code for these two files are in [run_attack_classification.py](https://github.com/jind11/TextFooler/blob/master/run_attack_classification.py) and [run_attack_nli.py](https://github.com/jind11/TextFooler/blob/master/run_attack_nli.py). Here we explain each required argument in details:
+
+  * --dataset_path: The path to the dataset. We put the 1000 examples for each dataset we used in the paper in the folder [data](https://github.com/jind11/TextFooler/tree/master/data).
+  * --target_model: Name of the target model such as ''bert''.
+  * --target_model_path: The path to the trained parameters of the target model. For ease of replication, we shared the [trained BERT model parameters](https://drive.google.com/drive/folders/1wKjelHFcqsT3GgA7LzWmoaAHcUkP4c7B?usp=sharing), the [trained LSTM model parameters](https://drive.google.com/drive/folders/108myH_HHtBJX8MvhBQuvTGb-kGOce5M2?usp=sharing), and the [trained CNN model parameters](https://drive.google.com/drive/folders/1Ifowzfers0m1Aw2vE8O7SMifHUhkTEjh?usp=sharing) on each dataset we used.
+  * --counter_fitting_embeddings_path: The path to the counter-fitting word embeddings.
+  * --counter_fitting_cos_sim_path: This is optional. If given, then the pre-computed cosine similarity scores based on the counter-fitting word embeddings will be loaded to save time. If not, it will be calculated.
+  * --USE_cache_path: The path to save the USE model file (Downloading is automatic if this path is empty).
+  
+Two more things to share with you:
+
+1. In case someone wants to replicate our experiments for training the target models, we shared the used [seven datasets](https://drive.google.com/open?id=1N-FYUa5XN8qDs4SgttQQnrkeTXXAXjTv) we have processed for you!
+
+2. In case someone may want to use our generated adversary results towards the benchmark data directly, [here it is](https://drive.google.com/drive/folders/12yeqcqZiEWuncC5zhSUmKBC3GLFiCEaN?usp=sharing).
---
+++ deberta.py
@@ -0,0 +1,122 @@
+
+import os
+import torch
+
+from DeBERTa.DeBERTa.deberta.bert import *
+from DeBERTa.DeBERTa.deberta.config import ModelConfig
+
+
+class DeBERTaReconfig(torch.nn.Module):
+    def __init__(self, model_path, model_config_path):
+        super().__init__()
+        state = torch.load(model_path, map_location='cpu')
+        if 'config' in state:
+            config = ModelConfig.from_dict(state['config'])
+        else:
+            config = ModelConfig.from_json_file(model_config_path)
+
+        self.embeddings = BertEmbeddings(config)
+        self.encoder = BertEncoder(config)
+        self.config = config
+        self.pre_trained = model_path
+        self.apply_state(state)
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None, output_all_encoded_layers=True,
+                position_ids=None, return_att=False):
+        """
+        Args:
+          input_ids:
+            a torch.LongTensor of shape [batch_size, sequence_length] \
+          with the word token indices in the vocabulary
+
+          attention_mask:
+            an optional parameter for input mask or attention mask.
+
+            - If it's an input mask, then it will be torch.LongTensor of shape [batch_size, sequence_length] with indices \
+          selected in [0, 1]. It's a mask to be used if the input sequence length is smaller than the max \
+          input sequence length in the current batch. It's the mask that we typically use for attention when \
+          a batch has varying length sentences.
+
+            - If it's an attention mask then it will be torch.LongTensor of shape [batch_size, sequence_length, sequence_length]. \
+          In this case, it's a mask indicate which tokens in the sequence should be attended by other tokens in the sequence.
+
+          token_type_ids:
+            an optional torch.LongTensor of shape [batch_size, sequence_length] with the token \
+          types indices selected in [0, 1]. Type 0 corresponds to a `sentence A` and type 1 corresponds to \
+          a `sentence B` token (see BERT paper for more details).
+
+          output_all_encoded_layers:
+            whether to output results of all encoder layers, default, True
+
+        Returns:
+
+          - The output of the stacked transformer layers if `output_all_encoded_layers=True`, else \
+          the last layer of stacked transformer layers
+
+          - Attention matrix of self-attention layers if `return_att=True`
+
+
+        Example::
+
+          # Batch of wordPiece token ids.
+          # Each sample was padded with zero to the maxium length of the batch
+          input_ids = torch.LongTensor([[31, 51, 99], [15, 5, 0]])
+          # Mask of valid input ids
+          attention_mask = torch.LongTensor([[1, 1, 1], [1, 1, 0]])
+
+          # DeBERTa model initialized with pretrained base model
+          bert = DeBERTa(pre_trained='base')
+
+          encoder_layers = bert(input_ids, attention_mask=attention_mask)
+
+        """
+
+        if attention_mask is None:
+            attention_mask = torch.ones_like(input_ids)
+        if token_type_ids is None:
+            token_type_ids = torch.zeros_like(input_ids)
+
+        embedding_output = self.embeddings(input_ids.to(torch.long), token_type_ids.to(torch.long), position_ids,
+                                           attention_mask)
+        encoded_layers = self.encoder(embedding_output,
+                                      attention_mask,
+                                      output_all_encoded_layers=output_all_encoded_layers, return_att=return_att)
+        if return_att:
+            encoded_layers, att_matrixs = encoded_layers
+
+        if not output_all_encoded_layers:
+            encoded_layers = encoded_layers[-1:]
+
+        if return_att:
+            return encoded_layers, att_matrixs
+        return encoded_layers
+
+    def apply_state(self, state=None):
+        """ Load state from previous loaded model state dictionary.
+
+          Args:
+            state (:obj:`dict`, optional): State dictionary as the state returned by torch.module.state_dict(), default: `None`. \
+                If it's `None`, then will use the pre-trained state loaded via the constructor to re-initialize \
+                the `DeBERTa` model
+        """
+        if self.pre_trained is None and state is None:
+            return
+
+        def key_match(key, s):
+            c = [k for k in s if key in k]
+            assert len(c) == 1, c
+            return c[0]
+
+        current = self.state_dict()
+        print('needs to be (state_dict post BERT config): ' + str(len(current)))
+        print('is (model state): ' + str(len(state.keys())))
+        # TODO: figure out why the layers aren't the same. skipping for now.
+        #for c in current.keys():
+        #    current[c] = state[key_match(c, state.keys())]
+        self.load_state_dict(current)
+
+
+class DeBERTaTxtClassifier(torch.nn.Module):
+    def __init__(self, model_path, model_config_path):
+        super().__init__()
+        self.deberta = DeBERTaReconfig(model_path, model_config_path)
